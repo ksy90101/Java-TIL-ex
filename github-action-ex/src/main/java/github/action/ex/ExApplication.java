4a89200547e728f43ebde1f20ed32af0fdc6ba40
SeYun <ksy90101@naver.com>
2020-08-07 21:41:04 +0900

    github Action 예제 project init

---
+++ github-action-ex/src/main/java/github/action/ex/ExApplication.java
@@ -0,0 +1,13 @@
+package github.action.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExApplication.class, args);
+    }
+
+}
---
+++ github-action-ex/src/main/java/github/action/ex/domian/User.java
@@ -0,0 +1,4 @@
+package github.action.ex.domian;
+
+public class User {
+}
