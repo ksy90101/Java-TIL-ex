b0aa23287576e5f38b8067c87eaef6b4dd184432
SeYun <ksy90101@naver.com>
2020-08-07 21:47:30 +0900

    테스트 실패

---
+++ github-action-ex/src/test/java/github/action/ex/domian/UserTest.java
@@ -0,0 +1,17 @@
+package github.action.ex.domian;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @DisplayName("생성자 확인")
+    @Test
+    void constructorTest() {
+        User user = new User("럿고", 28);
+        assertThat(user.getAge()).isEqualTo(30);
+    }
+}
